8e4576ea12d3d20bd7f4c913fd3ac6f657aa69a6
Stefan Musarra <stefan0@stefan0.com>
2025-10-31 12:53:26 -0700

    Add all necessary JavaScript for collapsible nabvar

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Get the navbar collapse element
   const navbarCollapse = document.querySelector(".navbar-collapse");
 
-  // Get all navbar links
-  // const navbarLinks = document.querySelectorAll(".navbar-collapse a");
+  // Get all navbar links including the navbar-brand
   const navbarLinks = document.querySelectorAll(".my-nav a");
 
   // Add an event listener to each navbar link
@@ -18,6 +17,24 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       // Add the "active" class to the clicked navbar link
       event.target.classList.add("active");
+      // Stop the event from propagating to Bootstrap event handlers
+      event.stopPropagation();
     }),
   );
+
+  // Add an event listener to the navbar toggle button to handle the click event
+  document
+    .querySelector(".navbar-toggler")
+    .addEventListener("click", (event) => {
+      // When the navbar toggle button is clicked, add the "show" class to the navbar collapse element
+      navbarCollapse.classList.toggle("show");
+      // Stop the event from propagating to the body event handler
+      event.stopPropagation();
+    });
+
+  // Add an event listener to the body to handle the click event
+  document.body.addEventListener("click", () => {
+    // When the body is clicked, remove the "show" class from the navbar collapse element
+    navbarCollapse.classList.remove("show");
+  });
 });
